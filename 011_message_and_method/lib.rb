e143f98b03cf710c7752e1a5168ab6934faab976
David Begin <davidmichaelbe@gmail.com>
2015-07-01 17:23:31 -0700

    a good example of the pitfalls of method objects

--- 011_message_and_method/lib.rb
+++ 011_message_and_method/lib.rb
@@ -1,12 +1,47 @@
+module Plugins
+  class Beep
+    def initialize(teaclock)
+      teaclock.ui.extend(UiWithBeep)
+    end
+
+    module UiWithBeep
+      def notify(msg)
+        puts "BEEP!"
+        super
+      end
+    end
+  end
+end
+
 class TeaClock
   attr_accessor :timer
   attr_accessor :ui
+
+  def initialize(seconds)
+    @ui = StdioUi.new
+    @timer = SleepTimer.new(seconds, ui)
+    init_plugins
+  end
+
+  def start
+    timer.start
+  end
+
+  private
+
+  def init_plugins
+    @plugins = []
+    ::Plugins.constants.each do |name|
+      puts "initializing #{name} Plugin"
+      @plugins << ::Plugins.const_get(name).new(self)
+    end
+  end
 end
 
 SleepTimer = Struct.new(:seconds, :notifier) do
   def start
     sleep seconds
-    notifier.call("Tea is ready!")
+    notifier.notify("Tea is ready!")
   end
 end
 
@@ -25,9 +60,7 @@ end
 # end
 # notifier_instance = Notifier.new
 # notifier = notifier_instance.method(:notify)
-
-timer = SleepTimer.new(1, notifier)
-
+# timer = SleepTimer.new(1, notifier)
 # timer.start
 
 class StdioUi
@@ -35,3 +68,5 @@ class StdioUi
     puts text
   end
 end
+
+TeaClock.new(1).start
